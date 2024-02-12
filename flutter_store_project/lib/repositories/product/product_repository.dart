943f0b381477ea454918743aa6cdaa0a0ed8d398
Manuel Molina García <mmgmolina03@gmail.com>
2024-02-12 08:24:52 +0100

    progress

--- flutter_store_project/lib/repositories/product/product_repository.dart
+++ flutter_store_project/lib/repositories/product/product_repository.dart
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import 'package:flutter_store_project/models/product_response/product_response.dart';
 
 abstract class ProductRepo {
   Future<ProductResponse> fetchProduct();
   Future<ProductResponse> fetchSingleProduct(int productId);
 }
-=======
-
->>>>>>> a5aa38d1e0c006fa63d536b4464ce87337eef622
